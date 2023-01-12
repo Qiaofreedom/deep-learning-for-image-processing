6840da1004a2dbff13757f597c6aad7333546566
Qiaofreedom <103122122+Qiaofreedom@users.noreply.github.com>
2023-01-12 22:19:38 +0100

    Update model.py

--- pytorch_classification/Test3_vggnet/model.py
+++ pytorch_classification/Test3_vggnet/model.py
@@ -12,8 +12,8 @@ model_urls = {
 
 class VGG(nn.Module):
     def __init__(self, features, num_classes=1000, init_weights=False):
-        super(VGG, self).__init__()
-        self.features = features
+        super(VGG, self).__init__()  #super用于执行父类的INT方法
+        self.features = features  #自定义的特征提取结构
         self.classifier = nn.Sequential(
             nn.Linear(512*7*7, 4096),
             nn.ReLU(True),
@@ -30,16 +30,16 @@ class VGG(nn.Module):
         # N x 3 x 224 x 224
         x = self.features(x)
         # N x 512 x 7 x 7
-        x = torch.flatten(x, start_dim=1)
+        x = torch.flatten(x, start_dim=1) # 特征提取完后先展平。展平完再分类
         # N x 512*7*7
         x = self.classifier(x)
         return x
 
     def _initialize_weights(self):
-        for m in self.modules():
+        for m in self.modules(): # 遍历网络的每一个子模块
             if isinstance(m, nn.Conv2d):
                 # nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                nn.init.xavier_uniform_(m.weight)
+                nn.init.xavier_uniform_(m.weight) #初始化卷积核权重
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
             elif isinstance(m, nn.Linear):
@@ -58,8 +58,9 @@ def make_features(cfg: list):
             conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
             layers += [conv2d, nn.ReLU(True)]
             in_channels = v
-    return nn.Sequential(*layers)
+    return nn.Sequential(*layers) # 将列表通过非关键字的参数传进去
 
+#配置列表
 
 cfgs = {
     'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
@@ -69,7 +70,7 @@ cfgs = {
 }
 
 
-def vgg(model_name="vgg16", **kwargs):
+def vgg(model_name="vgg16", **kwargs): # **kwargs 指可变长度的字典变量
     assert model_name in cfgs, "Warning: model number {} not in cfgs dict!".format(model_name)
     cfg = cfgs[model_name]
 
