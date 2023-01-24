16771f74783e0b462faf8cad1bbacff95186f008
Qiaofreedom <103122122+Qiaofreedom@users.noreply.github.com>
2023-01-24 13:06:19 +0100

    Update train_and_eval.py

--- pytorch_segmentation/fcn/train_utils/train_and_eval.py
+++ pytorch_segmentation/fcn/train_utils/train_and_eval.py
@@ -5,11 +5,12 @@ import train_utils.distributed_utils as utils
 
 def criterion(inputs, target):
     losses = {}
-    for name, x in inputs.items():
+    for name, x in inputs.items(): # inputs会返回一个模型预测的结果。返回主输出和辅助输出上的结果
         # 忽略target中值为255的像素，255的像素是目标边缘或者padding填充
-        losses[name] = nn.functional.cross_entropy(x, target, ignore_index=255)
+        losses[name] = nn.functional.cross_entropy(x, target, ignore_index=255) #x是网络预测结果，target是预测标签，ignore_index是将target中像素值255的都忽略
+       
 
-    if len(losses) == 1:
+    if len(losses) == 1: #说明没有使用辅助分类器，只有主分类器
         return losses['out']
 
     return losses['out'] + 0.5 * losses['aux']
@@ -17,16 +18,16 @@ def criterion(inputs, target):
 
 def evaluate(model, data_loader, device, num_classes):
     model.eval()
-    confmat = utils.ConfusionMatrix(num_classes)
+    confmat = utils.ConfusionMatrix(num_classes) #创建一个混淆矩阵
     metric_logger = utils.MetricLogger(delimiter="  ")
     header = 'Test:'
     with torch.no_grad():
-        for image, target in metric_logger.log_every(data_loader, 100, header):
+        for image, target in metric_logger.log_every(data_loader, 100, header): # 遍历验证集数据 data_loader
             image, target = image.to(device), target.to(device)
             output = model(image)
             output = output['out']
 
-            confmat.update(target.flatten(), output.argmax(1).flatten())
+            confmat.update(target.flatten(), output.argmax(1).flatten()) # 1对应channel
 
         confmat.reduce_from_all_processes()
 
@@ -39,27 +40,27 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, lr_scheduler, 
     metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
     header = 'Epoch: [{}]'.format(epoch)
 
-    for image, target in metric_logger.log_every(data_loader, print_freq, header):
+    for image, target in metric_logger.log_every(data_loader, print_freq, header): # 通过for循环，遍历data_loader
         image, target = image.to(device), target.to(device)
         with torch.cuda.amp.autocast(enabled=scaler is not None):
             output = model(image)
             loss = criterion(output, target)
 
-        optimizer.zero_grad()
+        optimizer.zero_grad() #清空优化器历史梯度
         if scaler is not None:
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()
         else:
-            loss.backward()
-            optimizer.step()
+            loss.backward() #误差反向传播
+            optimizer.step() #更新参数
 
-        lr_scheduler.step()
+        lr_scheduler.step() #调用更新学习率策略 的step方法
 
-        lr = optimizer.param_groups[0]["lr"]
+        lr = optimizer.param_groups[0]["lr"] #将学习率提取出来
         metric_logger.update(loss=loss.item(), lr=lr)
 
-    return metric_logger.meters["loss"].global_avg, lr
+    return metric_logger.meters["loss"].global_avg, lr #训练的平均损失
 
 
 def create_lr_scheduler(optimizer,
