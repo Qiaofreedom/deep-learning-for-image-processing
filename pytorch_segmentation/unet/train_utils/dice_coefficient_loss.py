d41e305e76bed93662a73daebbf71a24422fcbde
Qiaofreedom <103122122+Qiaofreedom@users.noreply.github.com>
2023-01-25 21:19:17 +0100

    Update dice_coefficient_loss.py

--- pytorch_segmentation/unet/train_utils/dice_coefficient_loss.py
+++ pytorch_segmentation/unet/train_utils/dice_coefficient_loss.py
@@ -19,39 +19,40 @@ def build_target(target: torch.Tensor, num_classes: int = 2, ignore_index: int =
 
 
 def dice_coeff(x: torch.Tensor, target: torch.Tensor, ignore_index: int = -100, epsilon=1e-6):
+    #x是针对某一个类别的预测矩阵，target是针对某一个类别的GT，ignore_index是哪些数值区域我们需要忽略
     # Average of Dice coefficient for all batches, or for a single mask
     # 计算一个batch中所有图片某个类别的dice_coefficient
     d = 0.
-    batch_size = x.shape[0]
+    batch_size = x.shape[0] 
     for i in range(batch_size):
-        x_i = x[i].reshape(-1)
+        x_i = x[i].reshape(-1) #第I张图片
         t_i = target[i].reshape(-1)
-        if ignore_index >= 0:
+        if ignore_index >= 0: 
             # 找出mask中不为ignore_index的区域
-            roi_mask = torch.ne(t_i, ignore_index)
-            x_i = x_i[roi_mask]
-            t_i = t_i[roi_mask]
-        inter = torch.dot(x_i, t_i)
+            roi_mask = torch.ne(t_i, ignore_index) # 寻找 t_i中不为255的区域
+            x_i = x_i[roi_mask] #提取预测中感兴趣的区域
+            t_i = t_i[roi_mask] #提取对应target中感兴趣的区域
+        inter = torch.dot(x_i, t_i) #内积操作
         sets_sum = torch.sum(x_i) + torch.sum(t_i)
         if sets_sum == 0:
             sets_sum = 2 * inter
 
         d += (2 * inter + epsilon) / (sets_sum + epsilon)
 
-    return d / batch_size
+    return d / batch_size # 计算一个batch中所有图片某个类别的dice_coefficient
 
 
 def multiclass_dice_coeff(x: torch.Tensor, target: torch.Tensor, ignore_index: int = -100, epsilon=1e-6):
     """Average of Dice coefficient for all classes"""
     dice = 0.
     for channel in range(x.shape[1]):
-        dice += dice_coeff(x[:, channel, ...], target[:, channel, ...], ignore_index, epsilon)
+        dice += dice_coeff(x[:, channel, ...], target[:, channel, ...], ignore_index, epsilon) #遍历每个类别的dice_coeff。因为one_hot已经生成了 总类别个数 的GT
 
-    return dice / x.shape[1]
+    return dice / x.shape[1] #得到左右类别的 dice_coeff 均值
 
 
 def dice_loss(x: torch.Tensor, target: torch.Tensor, multiclass: bool = False, ignore_index: int = -100):
     # Dice loss (objective to minimize) between 0 and 1
-    x = nn.functional.softmax(x, dim=1)
+    x = nn.functional.softmax(x, dim=1) # 在channel方向做softmax处理。计算每个像素针对每个类别的概率
     fn = multiclass_dice_coeff if multiclass else dice_coeff
-    return 1 - fn(x, target, ignore_index=ignore_index)
+    return 1 - fn(x, target, ignore_index=ignore_index) #得到当前batch这个数据的 dice_coeff
